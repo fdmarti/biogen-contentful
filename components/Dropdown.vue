1dac571762ec4791dc5ff0ae0f53265501dda3c8
cl4ram <molinayvediaclara@outlook.com>
2022-06-01 11:02:36 -0300

    call to dropdown data

---
+++ components/Dropdown.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="categories">
+        <span 
+            v-for="(categorie,index) in categories" 
+            :key="index"
+            class="text-lg pr-5 text-blue-500"
+            >
+            {{categorie.fields.title}}
+        </span>
+    </div>
+    <div v-else>
+        <span class="text-lg pr-5 text-blue-500"> There is no categories in this blog</span>
+    </div>
+</template>
+<script>
+export default{
+    props : ['categories']
+}
+
+const contentful = require('contentful')
+const client = contentful.createClient({
+  space: 'f4cwegc8dwoa',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: '5roN7oSggUAq--DJxECO0XGC-UXCBzBKwVSFRcO6Tf8'
+})
+client.getEntry('4prKyCBmEikaFurk1mlWT0').then(function (entry) {
+  console.log(entry.fields.label);
+});
+</script>
