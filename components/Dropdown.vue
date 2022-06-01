859346f3019ce95166dc1393576e56d9cd22a60a
cl4ram <molinayvediaclara@outlook.com>
2022-06-01 17:00:45 -0300

    dropdown styles

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="form-group">
-            <label for="country">{{dropdownData}}</label>
+        <div class="form">
+            <div class="input">
+                <label for="country">{{dropdownTitle}}</label>
+                <select class="form-control">
+                    <option :value="country" v-for="(country, index) in dropdownData" :key="country" :disabled="index === 0" :selected="index === 0" required>{{country}}</option>
+                </select>
+                <div class="arrow"></div>
+            </div>
         </div>
+    </div>
 </template>
 
 <script>
@@ -12,20 +20,69 @@
         name: "dropdownComponent",
         data() {
             return {
+                dropdownTitle : [],
                 dropdownData : []
             };
         },
         mounted() {
-            console.log(this.getPosts());
+            console.log(this.getCountries());
         },
         methods: {
-            async getPosts() {
+            async getCountries() {
                 await client.getEntry('4prKyCBmEikaFurk1mlWT0')
-                    .then(function (entry) {
-                        console.log(entry.fields.title);
-                        this.dropdownData  = entry.fields.label
+                    .then(entry => {
+                    this.dropdownTitle  = entry.fields.title
+                    this.dropdownData = entry.fields.label
                     });
-            }
+            },
         },
     }
 </script>
+
+<style scoped>
+    .form{
+        width: 720px;
+    }
+    .form-group{
+        display: flex;
+        justify-content: center;
+    }
+    label{
+        color: rgb(112, 112, 112);
+        font: 300 12px / 15px Poppins, "Open Sans", Arial, sans-serif; 
+        display: block;
+        margin-bottom: 5px;
+    }
+    .input{
+        min-height: 66px;
+    }
+    .input::before{
+        content: '';
+        position: relative;
+        left: 100%;
+        top: 61px;
+        width: 0; 
+        height: 0; 
+        border-left: 5px solid transparent;
+        border-right: 5px solid transparent;
+        border-top: 5px solid #707070;
+        margin-left: -21px;
+    }
+    select{
+        min-width: 100%;
+        color: rgb(112, 112, 112);
+        font: 300 14px / 15px Poppins, "Open Sans", Arial, sans-serif; 
+        background-color: rgb(255, 255, 255);
+        background-clip: padding-box;
+        border: 1px solid rgb(206, 212, 218);
+        border-radius: 0.25rem;
+        height: 32px;
+        padding-left: 10px;
+        appearance: none;
+    }
+
+    option:disabled {
+    color: -internal-light-dark(gretext, rgb(170, 170, 170));
+}
+
+</style>
