6fb23dfe2ea2414ae2f420d0a6acd7250f340d7a
Federico <fdmartinez44@gmail.com>
2022-06-03 16:41:14 -0300

    se acomodo el puto richtext

--- components/Richtext.vue
+++ components/Richtext.vue
@@ -1,30 +1,36 @@
 <template>
     <div v-if="dataRichtext" class="form-group">
-    <section>
-        <div v-for="(field, index) in dataRichtext.fields.content.content" :key="index" class="form">
-
-        {{field}}
-            <!-- <a :href="field.data.uri" target="_blank" v-if="field.data.uri">{{field.data.uri}}</a>
-            {{field.value}} -->
-        </div>
-    </section>
+        <section class="text-group">
+            <RichTextRenderer :document="dataRichtext.fields.content" class="form"/>
+        </section>
     </div>
 </template>
 <script>
+import RichTextRenderer from 'contentful-rich-text-vue-renderer';
+
 export default {
-    props: ["dataRichtext"]
+    props: ["dataRichtext"],
+    components: {RichTextRenderer}
 }
 </script>
 <style scoped>
-    .form{
-        width: 720px;
+    p{
         color: rgb(112, 112, 112);
-        font: 300 12px / 15px Poppins, "Open Sans", Arial, sans-serif; 
+        font: 12px / 14px Poppins, "Open Sans", Arial, sans-serif;
+        margin-bottom: 2rem;
+    }
+
+    a{
+        text-decoration: underline;
     }
 
     .form-group{
         display: flex;
         justify-content: center;
-        padding: 0.1rem 0;
+    }
+
+    .text-group{
+        width: 720px;
+        justify-content: center;
     }
 </style>
